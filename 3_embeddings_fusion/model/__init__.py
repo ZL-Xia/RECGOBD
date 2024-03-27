880c32d574eb8e6f3e36f54a661e06b0350b5857
ZL-Xia <zl.xia@siat.ac.cn>
2024-03-27 22:25:18 +0800

    change commit

--- 3_embeddings_fusion/model/__init__.py
+++ 3_embeddings_fusion/model/__init__.py
@@ -6,6 +6,6 @@ from . import model
 
 import tensorflow as tf
 
-model_dict = {'DeepAtt': model.DeepAtt, 'DeepAttPlus': model.DeepAttPlus, 'DeepSEA': model.DeepSEA,
-              'DanQ': model.DanQ, 'DanQ_JASPAR': model.DanQ_JASPAR,'DeepAtt_3_embedding': model.DeepAtt_3_embedding}
+model_dict = {'RecGOBD_1_embedding':model.RecGOBD_1_embedding,'RecGOBD_2_embeddings':model.RecGOBD_2_embeddings,
+             'RecGOBD_3_embeddings':model.RecGOBD_3_embeddings,'RecGOBD_4_embeddings':model.RecGOBD_4_embeddings}
 loss_dict = {'NLL': tf.keras.losses.BinaryCrossentropy, 'Focal': loss.BinaryFocalloss}
