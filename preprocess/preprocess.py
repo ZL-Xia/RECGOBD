4b6d5b1d3bfcdd9eef81ea4c81eae33674c1a5b4
ZL-Xia <zl.xia@siat.ac.cn>
2024-03-29 12:47:49 +0800

    second commit

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,59 +1,4 @@
 # -*- coding: utf-8 -*-
-# import h5py
-# import numpy as np
-# import tensorflow as tf
-# import scipy.io as sio
-# # from mat4py import mat4py
-# from tqdm import tqdm
-
-
-# def serialize_example(x, y):
-#     # Create a dictionary mapping the feature name to the tf.Example-compatible data type.
-#     example = {
-#         'x': tf.train.Feature(int64_list=tf.train.Int64List(value=x.flatten())),
-#         'y': tf.train.Feature(int64_list=tf.train.Int64List(value=y.flatten()))}
-
-#     # Create a Features message using tf.train.Example.
-#     example = tf.train.Features(feature=example)
-#     example = tf.train.Example(features=example)
-#     serialized_example = example.SerializeToString()
-#     return serialized_example
-
-# def traindata_to_tfrecord():
-#     filename = './data/train.mat'
-#     with h5py.File(filename, 'r') as file:
-#         x = file['trainxdata'] # shape = (1000, 4, 4400000)
-#         y = file['traindata'] # shape = (919, 4400000)
-#         x = np.transpose(x, (2, 0, 1)) # shape = (4400000, 1000, 4)
-#         y = np.transpose(y, (1, 0)) # shape = (4400000, 919)
-
-#     for file_num in range(4):
-#         with tf.io.TFRecordWriter('./data/traindata-%.2d.tfrecord' % file_num) as writer:
-#             for i in tqdm(range(file_num*1100000, (file_num+1)*1100000), desc="Processing Train Data {}".format(file_num), ascii=True):
-#                 example_proto = serialize_example(x[i], y[i])
-#                 writer.write(example_proto)
-
-  
-# def testdata_to_tfrecord():
-#     filename = './data/test.mat' 
-#     data = sio.loadmat(filename)
-#     # print(data)
-#     x = data['testxdata'] # shape = (455024, 4, 1000)
-#     y = data['testdata'] # shape = (455024, 919)
-#     x = np.transpose(x, (0, 2, 1)) # shape = (455024, 1000, 4)
-#     y = np.transpose(y, (0, 1)) # shape = (455024, 919)
-
-#     with tf.io.TFRecordWriter('./data/testdata.tfrecord') as writer:
-#         for i in tqdm(range(len(y)), desc="Processing Test Data", ascii=True):
-#             example_proto = serialize_example(x[i], y[i])
-#             writer.write(example_proto)
-
-# if __name__ == '__main__':
-#     # Write the train data and test data to .tfrecord file.
-#     traindata_to_tfrecord()
-#     testdata_to_tfrecord()
-
-
 import os
 import tensorflow as tf
 import numpy as np
@@ -109,7 +54,6 @@ class MyDataset:
         train_dataset = dataset.take(train_size)
         val_dataset = dataset.skip(train_size).take(val_size)
         test_dataset = dataset.skip(train_size + val_size).take(test_size)
-        train_dataset = train_dataset.shuffle(buffer_size)
 
         # train_dataset = train_dataset.batch(batch_size)
         # train_dataset = train_dataset.prefetch(prefetch_size)
@@ -143,11 +87,8 @@ class MyDataset:
 
 if __name__ == '__main__':
     # Load data
-    npz_file = '/home/xiazhiliang/Project/Bioinfor-DeepATT-main/mydata/try_esm2_3B_2200_10label.npz'
-    csv_file = '/home/xiazhiliang/Project/Bioinfor-DeepATT-main/mydata/s_prot_func_2200_10label.csv'   #prot_func.csv   
-    # label_columns = ['GO0001666', 'GO0002250', 'GO0006954','GO0006955','GO0006974','GO0007399','GO0007420','GO0042493','GO0045087']  #9label
-    # label_columns = ['GO0001666','GO0002250','GO0004984','GO0006954','GO0006955','GO0006974','GO0006979','GO0007399','GO0007417','GO0007420','GO0009615','GO0032355','GO0032496','GO0032869','GO0042493','GO0042742','GO0045087','GO0045471','GO0051607','GO0071222','GO0071260','GO0071277','GO0071356','GO0071456','GO0150104']  #25label
-   # label_columns = ['GO0002250','GO0004984','GO0006955','GO0007417','GO0007420','GO0042742','GO0045087','GO0045471','GO0051607','GO0071277','GO0071356','GO0071456','GO0150104']  #13label
+    npz_file = '/home/Project/mydata/try_esm2_3B_2200_10label.npz'
+    csv_file = '/home/Project/mydata/s_prot_func_2200_10label.csv'   #prot_func.csv   
     label_columns = ['GO0002250','GO0004984','GO0006955','GO0007420','GO0042742','GO0045087','GO0045471','GO0071277','GO0071456','GO0150104'] #10label
     # Call create_dataset function
-    MyDataset.create_dataset(npz_file=npz_file, csv_file=csv_file,label_columns=label_columns,save_dir='/home/xiazhiliang/Project/Bioinfor-DeepATT-main/mydata/')
+    MyDataset.create_dataset(npz_file=npz_file, csv_file=csv_file,label_columns=label_columns,save_dir='/home/Project/mydata/')
